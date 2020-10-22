ff9e0b9a63f36e5661c70872905c700d3841267e
Mohammad Alshoubaki <mohammad.z.alshoubaki@gmail.com>
2020-10-22 09:47:04 -0400

    Add project files.

---
+++ ClassesAndObjects/DatingProfile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndObjects
+{
+ 
+
+        class Profile
+        {
+            private string name;
+            private int age;
+            private string city;
+            private string country;
+            private string pronouns;
+            private string[] hobbies;
+
+            public Profile(string name, int age, string city, string country, string pronouns = "they/them")
+            {
+
+                this.name = name;
+                this.age = age;
+                this.city = city;
+                this.country = country;
+                this.pronouns = pronouns;
+
+            }
+            public string ViewProfile()
+            {
+
+                string hobbiesString = "";
+                foreach (string i in this.hobbies)
+                {
+                    hobbiesString += $"{i}\n";
+
+                }
+                return $"Name: {this.name}, age: {this.age}, city: {this.city}, country: {this.country}, pronouns: {this.pronouns}\nHobbies:\n{hobbiesString}";
+            }
+            public void SetHobbies(string[] hobbies)
+            {
+                this.hobbies = hobbies;
+            }
+        }
+    
+}
---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile sam = new Profile("Sam Drakkila", 30, "New York", "USA", "he/him");
+            sam.SetHobbies(new string[] { "hobby1", "hobby2"});
+            Console.WriteLine(sam.ViewProfile());
+        }
+    }
+}
